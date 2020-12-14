5bd39181989b9fb06861a1eb7613e40e42a87c1a
aboubakar20 <aboubakar@rebeltechnology.io>
2020-12-14 12:39:53 +0500

    add go button

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Test App</h1>
+    <button>Go</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
 
 <style>
@@ -24,5 +19,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+button {
+  font-size: 20px;
+  padding: 5px 20px;
 }
 </style>
