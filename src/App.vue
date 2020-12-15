a2f7220ab49563ebe8fc1588259e5657c6c74c8d
aboubakar20 <aboubakar@rebeltechnology.io>
2020-12-15 12:41:46 +0500

    filter and show posts in table

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,24 @@
   <div id="app">
     <h1>Test app</h1>
     <button @click="getPosts()">Go</button>
-    <p>{{ posts }}</p>
+    <table>
+      <thead>
+        <tr>
+          <th>Sr#</th>
+          <th>Author</th>
+          <th>Title</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(post, index) in posts" :key="index">
+          <td>{{ index }}</td>
+          <td>{{ post.data.author_fullname }}</td>
+          <td>{{ post.data.title }}</td>
+          <td><button>Detail</button></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -12,7 +29,7 @@ export default {
   name: "App",
   data() {
     return {
-      posts: null,
+      posts: [],
     };
   },
   methods: {
@@ -42,5 +59,86 @@ export default {
 button {
   font-size: 20px;
   padding: 5px 20px;
+}
+table {
+  width: 750px;
+  border-collapse: collapse;
+  margin: 50px auto;
+}
+
+/* Zebra striping */
+tr:nth-of-type(odd) {
+  background: #eee;
+}
+
+th {
+  background: #3498db;
+  color: white;
+  font-weight: bold;
+}
+
+td,
+th {
+  padding: 10px;
+  border: 1px solid #ccc;
+  text-align: left;
+  font-size: 18px;
+}
+
+/* 
+Max width before this PARTICULAR table gets nasty
+This query will take effect for any screen smaller than 760px
+and also iPads specifically.
+*/
+@media only screen and (max-width: 760px),
+  (min-device-width: 768px) and (max-device-width: 1024px) {
+  table {
+    width: 100%;
+  }
+
+  /* Force table to not be like tables anymore */
+  table,
+  thead,
+  tbody,
+  th,
+  td,
+  tr {
+    display: block;
+  }
+
+  /* Hide table headers (but not display: none;, for accessibility) */
+  thead tr {
+    position: absolute;
+    top: -9999px;
+    left: -9999px;
+  }
+
+  tr {
+    border: 1px solid #ccc;
+  }
+
+  td {
+    /* Behave  like a "row" */
+    border: none;
+    border-bottom: 1px solid #eee;
+    position: relative;
+    padding-left: 50%;
+  }
+
+  td:before {
+    /* Now like a table header */
+    position: absolute;
+    /* Top/left values mimic padding */
+    top: 6px;
+    left: 6px;
+    width: 45%;
+    padding-right: 10px;
+    white-space: nowrap;
+    /* Label the data */
+    content: attr(data-column);
+
+    color: #000;
+    font-weight: bold;
+  }
 }
 </style>
